7195918028d4471c8d4497305dedbd4e8d0c93c0
leahmkh <105560625+leahmkh@users.noreply.github.com>
2024-06-09 10:36:04 -1000

    week 2

---
+++ week_2/quote.py
@@ -0,0 +1,11 @@
+"""
+Author: Leah Harris
+Date: 06/07/24
+File Name: quote.py
+Description: This file prints a quote from an author for
+WEB335 exercise 2.3
+"""
+
+quote = "The only thing we have to fear is fear itself. - Franklin D. Roosevelt"
+
+print(quote)
